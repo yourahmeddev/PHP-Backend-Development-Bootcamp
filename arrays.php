13fa1810992599619a79a01c0ca01575d6aeee65
yourahmeddev <ahmedhassansheikh7@gmail.com>
2024-01-05 00:38:15 +0500

    Hello world , Variables , arrays Done

---
+++ arrays.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $marks = array(12,30,40,50,100);
+    echo $marks[0];
+    $student = array("name"=>"Ahmed","age"=>22,"degree"=>"BS Software Enginnering");
+    echo $student["name"];
+    echo $student["age"];
+    echo $student["degree"];
+    ?>
+</body>
+</html>
---
+++ variables.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?PHP
+    $name = "Ahmed";
+    $age = 22;
+    $profession = "Software Engineer";
+    $programming = "PHP Developer";
+    echo $name;
+    echo $age;
+    echo $profession;
+    echo $programming;
+    ?>
+</body>
+</html>
